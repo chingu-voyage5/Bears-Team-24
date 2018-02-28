d9d620ed35726b88de94c6b54035174bd774dd20
Giovanni Sampaio <giovannibruno1@gmail.com>
2018-02-28 21:26:58 +0000

    fixed propTypes and methods declarations

--- frontend/src/Sidebar/Collapsible.js
+++ frontend/src/Sidebar/Collapsible.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import ProTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-class Collapsible extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      isExpanded: false 
-    }
-  }
-
-  handleToggle(e) {
-    e.preventDefault();
+class Collapsible extends React.Component { 
+  state = { isExpanded: false }
+  handleToggle = () => {
     this.setState({
       isExpanded: !this.state.isExpanded
     })
@@ -18,8 +11,9 @@ class Collapsible extends React.Component {
   render(){
     const {title, children} = this.props;
     const {isExpanded} = this.state;
+    
     return (
-      <div className={`panel ${isExpanded ? 'isExpanded' : ''}`} onClick={(e) => this.handleToggle(e)}>
+      <div className={`panel ${isExpanded ? 'isExpanded' : ''}`} onClick={this.handleToggle}>
         <div className="panel-heading">
           <h2>{title}</h2> 
         </div>
@@ -31,8 +25,8 @@ class Collapsible extends React.Component {
   }
 }
 
-Collapsible.ProTypes = {
-  title: ProTypes.string
+Collapsible.propTypes = {
+  title: PropTypes.string
 }
 
 export default Collapsible;
