31f191a4034e0678170dda791fb7e72cf5dc8b7b
keith1111 <keith1111@rambler.ru>
2018-03-01 12:25:59 +0300

    Rewrite *.js with Prettier

--- frontend/src/JSONHandle/JSONToHTML.js
+++ frontend/src/JSONHandle/JSONToHTML.js
@@ -1,42 +1,42 @@
-export default function JSONToHTML(tree){
-	//console.log(tree);
-	let resultHTML = '';
-	for (let i=0; i<tree.length; i++){
-		resultHTML += wrapTag(tree[i]);
-	}
-	
-	return resultHTML;
+export default function JSONToHTML(tree) {
+  //console.log(tree);
+  let resultHTML = "";
+  for (let i = 0; i < tree.length; i++) {
+    resultHTML += wrapTag(tree[i]);
+  }
+
+  return resultHTML;
 }
 
 const NON_ATTRIBUTES = ["tag", "textContent", "children"];
 
+function wrapTag(element) {
+  let innerContent = "";
 
-function wrapTag(element){
-	
-	let innerContent = "";
-	
-	if(element.children){
-		for(let j=0; j<element.children.length;j++){
-			innerContent += wrapTag(element.children[j]);
-		}
-	}
-	
-	let tag = element.tag;
-	let attributes = "";
-	for (let attr of Object.keys(element)){
-		if(NON_ATTRIBUTES.includes(attr)){
-			continue;
-		}
-		attributes += ` ${attr}="${element[attr]}"`;
-	}
-	let textContent = element.textContent || '' ;
-	innerContent += textContent;  	
-   
-   let template = element.tag ? `<${tag}${attributes}>${innerContent}</${tag}>` : innerContent;
-   // support <span> in text content. Need to rewrite. Any element will have "children" array but some of elements won't have a "tag".
-   // if with "tag" - look one level deeper. if no "tag": the "children" is array with 1 element, text-content.  
-   
-	console.log(template);
-	
-	return template;
+  if (element.children) {
+    for (let j = 0; j < element.children.length; j++) {
+      innerContent += wrapTag(element.children[j]);
+    }
+  }
+
+  let tag = element.tag;
+  let attributes = "";
+  for (let attr of Object.keys(element)) {
+    if (NON_ATTRIBUTES.includes(attr)) {
+      continue;
+    }
+    attributes += ` ${attr}="${element[attr]}"`;
+  }
+  let textContent = element.textContent || "";
+  innerContent += textContent;
+
+  let template = element.tag
+    ? `<${tag}${attributes}>${innerContent}</${tag}>`
+    : innerContent;
+  // support <span> in text content. Need to rewrite. Any element will have "children" array but some of elements won't have a "tag".
+  // if with "tag" - look one level deeper. if no "tag": the "children" is array with 1 element, text-content.
+
+  console.log(template);
+
+  return template;
 }
--- frontend/src/JSONHandle/index.js
+++ frontend/src/JSONHandle/index.js
@@ -1,3 +1,3 @@
-import JSONToHTML from './JSONToHTML';
+import JSONToHTML from "./JSONToHTML";
 
 export {JSONToHTML};
