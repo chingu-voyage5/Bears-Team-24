b916c7aec257d32275c17f68d310c56815b4cfd4
keith1111 <keith1111@rambler.ru>
2018-03-12 13:56:54 +0300

    Reconfigure link targets (UserList)

--- frontend/src/UserList/UserPage.js
+++ frontend/src/UserList/UserPage.js
@@ -13,7 +13,7 @@ export default class UserPage extends React.Component {
     let userInfo = (
       <p>
         User ID {this.props.userId} not found. Type &apos;/users&apos; in
-        address bar and hit Enter to see all registered users
+        address bar and hit Enter (or click the button below) to see all registered users
       </p>
     );
 
