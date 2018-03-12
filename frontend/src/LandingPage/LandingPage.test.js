9efad51852e0ffbcb8379ee110b9fdacfe9292a6
keith1111 <keith1111@rambler.ru>
2018-03-12 21:22:10 +0300

    Import jest-styled-components LandingPage.test

--- frontend/src/LandingPage/LandingPage.test.js
+++ frontend/src/LandingPage/LandingPage.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import LandingPage from './LandingPage';
+import 'jest-styled-components';
 
 test('renders LandingPage', () => {
   const pg = renderer.create(<LandingPage />).toJSON();
