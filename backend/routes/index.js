e6a5c146ec1c9d6d9e4778cf7521a290d540738f
nikrb <nik_rb@yahoo.com>
2018-03-17 16:06:11 +0000

    auth user endpoint and tidy

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,29 +7,16 @@ const passport = require('passport');
 
 const router = new Router();
 
-router.post('/api/v1/test',
-  auth.isLoggedIn,
-  (req, res) => {
-    res.json('wahoo');
-  });
+router.get('/api/v1/user/:id*?', auth.isLoggedIn, users.getDetail);
+router.get('/api/v1/users', auth.isLoggedIn, users.getAll);
 
-router.get('/api/v1/user/:id*?', users.getDetail);
-router.get('/api/v1/users', users.getAll);
-
-router.get('/auth/github',
-  (req, res, next) => {
-    console.log('github auth');
-    return next();
-  },
-  passport.authenticate('github') // , { scope: ['user:email'] }));
-);
+router.get('/auth/github', passport.authenticate('github'));
 
 router.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/' }),
   (req, res) => {
     // req.user gets populated by passport
-    console.log('/auth/github/callback', req.user);
-    res.redirect('http://localhost:3000');
+    res.redirect('http://127.0.0.1:3000');
   });
 
 router.post('/api/v1/register',
