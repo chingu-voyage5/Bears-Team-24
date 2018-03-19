863c6591ce3c0946c0c5242c9d423653aeaccedd
nikrb <nik_rb@yahoo.com>
2018-03-19 00:12:28 +0000

    refactor: top level FE app actions

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -1,25 +1,19 @@
 
-const getUser = () => {
-  return fetch('/api/v1/user', {
-    method: 'get',
-    headers: { 'content-type': 'application/json' },
-    credentials: 'same-origin'
-  })
-    .then(res => {
-      if (res.ok) {
-        return Promise.resolve(res.json());
-      }
-      return Promise.reject(res);
-    });
-};
+const getUser = () => fetch('/api/v1/user', {
+  method: 'get',
+  headers: { 'content-type': 'application/json' },
+  credentials: 'same-origin'
+}).then(res => {
+  if (res.ok) {
+    return Promise.resolve(res.json());
+  }
+  return Promise.reject(res);
+});
 
-const logout = () => {
-  return fetch('/api/v1/logout', {
-    method: 'post',
-    headers: { 'content-type': 'application/json' },
-    credentials: 'same-origin',
-  })
-    .then(res => res.json());
-};
+const logout = () => fetch('/api/v1/logout', {
+  method: 'post',
+  headers: { 'content-type': 'application/json' },
+  credentials: 'same-origin',
+}).then(res => res.json());
 
 export default { getUser, logout };
