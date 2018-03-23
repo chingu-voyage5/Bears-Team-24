e8fddd21b34f1b4595464994a0134afac3c6166a
nikrb <nik_rb@yahoo.com>
2018-03-23 22:20:04 +0000

    grab latest db user model

--- backend/models/user/user.js
+++ backend/models/user/user.js
@@ -1,41 +1,34 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const { Schema } = mongoose;
 mongoose.Promise = global.Promise;
-const validator = require('validator');
-const mongodbErrorHandler = require('mongoose-mongodb-errors');
-const passportLocalMongoose = require('passport-local-mongoose');
+// const mongodbErrorHandler = require('mongoose-mongodb-errors');
 
 /**
  * Passport-Local Mongoose will add a username,
  * hash and salt field to store the username,
  * the hashed password and the salt value.
  */
-// email can be anything in dev
-let emailSpec;
-if (process.env.NODE_ENV !== 'production') {
-  emailSpec = String;
-} else {
-  emailSpec = {
-    type: String,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    validate: [validator.isEmail, 'Invalid Email Address'],
-    required: 'Please Supply an email address'
-  };
-}
 const userSchema = new Schema({
-  active: Boolean,
-  email: emailSpec,
+  active: { type: Boolean, default: true },
+  created: { type: Date, default: Date.now },
+  github: {
+    id: String,
+    displayName: String,
+    username: String,
+  },
+  email: String,
   username: {
     type: String,
     required: 'Please supply a username',
-    trim: true
-  }
+    trim: true,
+  },
+  role: { type: String, enum: ['admin', 'member'], default: 'member' },
+  bio: String,
 });
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(mongodbErrorHandler);
+userSchema.plugin(passportLocalMongoose, { usernameField: 'username' });
+// userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model('User', userSchema);
