21021e5184c7385c8ace2852b84645ce9ad28eac
nikrb <nik_rb@yahoo.com>
2018-03-24 21:27:06 +0000

    add new backend route to get asset content (e.g. image)

--- backend/routes/assets/index.js
+++ backend/routes/assets/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable global-require */
 module.exports = {
   getAll: require('./getAll'),
   getDetail: require('./getDetail'),
+  getContent: require('./getContent'),
   upsert: require('./upsert'),
 };
