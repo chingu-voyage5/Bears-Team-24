93ee659aff959a719921a81b196bc0ec4f0fbb93
nikrb <nik_rb@yahoo.com>
2018-03-25 21:00:36 +0100

    fix: handle get asset endpoint not found

--- backend/routes/assets/getContent.js
+++ backend/routes/assets/getContent.js
@@ -2,8 +2,12 @@ const Asset = require('../../models/asset');
 
 const getContent = async (req, res) => {
   const asset = await Asset.findById(req.params.id);
-  res.setHeader('content-type', asset.content_type);
-  res.send(asset.content);
+  if (asset.content) {
+    res.setHeader('content-type', asset.content_type);
+    res.send(asset.content);
+  } else {
+    res.send('Image not found');
+  }
 };
 
 module.exports = getContent;
