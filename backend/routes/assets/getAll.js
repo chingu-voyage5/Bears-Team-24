c8b6a6e6cc6171d82c773e19ae407bead0b74ce2
nikrb <nik_rb@yahoo.com>
2018-03-25 00:17:34 +0000

    fix: lint errors

--- backend/routes/assets/getAll.js
+++ backend/routes/assets/getAll.js
@@ -1,13 +1,16 @@
 const Asset = require('../../models/asset');
 
 async function getAll(req, res) {
-  const assets = await Asset.find({}, {
-    content_type: 1,
-    owner: 1,
-    title: 1,
-    created:1
-  });
+  const assets = await Asset.find(
+    {},
+    {
+      content_type: 1,
+      owner: 1,
+      title: 1,
+      created: 1,
+    }
+  );
   res.json(assets);
-};
+}
 
 module.exports = getAll;
