0fdca988ebc6ac62534a01615a1627efbdda5793
nikrb <nik_rb@yahoo.com>
2018-03-25 02:11:23 +0100

    populate creator in be asset detail endpoint

--- backend/routes/assets/getDetail.js
+++ backend/routes/assets/getDetail.js
@@ -2,10 +2,10 @@ const Asset = require('../../models/asset');
 
 const getDetail = async (req, res) => {
   const assetId = req.params.id;
-  const asset = await Asset.findById(assetId);
+  const asset = await Asset.findById(assetId).populate('creator', 'username');
   // eslint-disable-next-line camelcase
-  const { title, description, content_type } = asset;
-  res.json({ title, description, content_type });
+  const { title, description, content_type, creator } = asset;
+  res.json({ title, description, content_type, creator });
 };
 
 module.exports = getDetail;
