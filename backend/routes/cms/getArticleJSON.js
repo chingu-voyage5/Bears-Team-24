7e12af414e4928d6a0cf7114f955ab2e0c5655f4
keith1111 <keith1111@rambler.ru>
2018-03-27 17:09:27 +0300

    Backend returns article JSON by ID from database

--- backend/routes/cms/getArticleJSON.js
+++ backend/routes/cms/getArticleJSON.js
@@ -1,5 +1,30 @@
-function getArticleJSON(req,res){
-	res.send(req.params);
+const Article = require('../../models/article');
+
+async function getArticleJSON(req,res){
+	// route /api/v1/articles/:id	
+	let {id} = req.params;
+	let query = {};
+	
+   
+   // valid or not Mongo ObjectID
+	if(id && id.match(/^[0-9a-fA-F]{24}$/)){
+		query = {"_id": id};
+	}		
+	
+	await Article.find(query)
+	.then(found => {
+		if(found.length == 0){
+			//not found specified id - show all
+			res.redirect('/api/v1/articles/');
+			return;
+		}		
+		
+			res.send(found);
+		
+		
+		});
+	
+	
 }
 
 module.exports = getArticleJSON;
