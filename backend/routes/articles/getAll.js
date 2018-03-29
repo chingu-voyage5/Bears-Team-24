0eeb756a45004ab199275bc187c5a6fcb224a17e
nikrb <nik_rb@yahoo.com>
2018-03-29 11:26:17 +0100

    specify fields to return from article get all

--- backend/routes/articles/getAll.js
+++ backend/routes/articles/getAll.js
@@ -1,7 +1,15 @@
 const Article = require('../../models/article');
 
 async function getAll(req, res) {
-  const articles = await Article.find().populate('creator', 'username');
+  const articles = await Article.find(
+    {},
+    {
+      topic: 1,
+      sub_topic: 1,
+      title: 1,
+      creator: 1,
+    }
+  ).populate('creator', 'username');
   res.json(articles);
 }
 
