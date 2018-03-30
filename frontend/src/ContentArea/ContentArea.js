e2a15383a59dd3a9309e771bf14796fed7e9da6e
keith1111 <keith1111@rambler.ru>
2018-03-30 12:45:56 +0300

    Remove unnecessary dependencies frontend/package.json

--- frontend/src/ContentArea/ContentArea.js
+++ frontend/src/ContentArea/ContentArea.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Redirect } from 'react-router';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import marked from 'marked';
 
 export default class ContentArea extends React.Component {
