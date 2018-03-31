14c9ffc12641a8c426e0b94fe8d6a996b92f31b8
nikrb <nik_rb@yahoo.com>
2018-03-31 14:10:23 +0100

    add FE AuthRoute

---
+++ frontend/src/AuthRoute.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+const propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  component: PropTypes.object,
+  redirect: PropTypes.string,
+  render: PropTypes.func,
+};
+const defaultProps = {
+  component: null,
+  redirect: '',
+  render: null,
+};
+
+const AuthRoute = ({
+  isLoggedIn,
+  component: Component,
+  redirect,
+  render: renderer,
+  ...rest
+}) => {
+  if (renderer && isLoggedIn) {
+    return <Route {...rest} render={renderer} />;
+  }
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isLoggedIn ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: redirect }} />
+        )
+      }
+    />
+  );
+};
+
+AuthRoute.propTypes = propTypes;
+AuthRoute.defaultProps = defaultProps;
+
+export default AuthRoute;
