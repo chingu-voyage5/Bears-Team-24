15ebd5fe3ab84c03b54f7a89774473c3f63dcf43
Viktors Jenovs <jviktors@gmail.com>
2018-03-31 14:55:26 +0300

    Refactor Articles to use Material-UI

--- frontend/src/Articles/Articles.js
+++ frontend/src/Articles/Articles.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// Material UI components
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableSortLabel,
+} from 'material-ui/Table';
+
 import actions from './actions';
-import Row, { dataNames } from './Row';
-import {
-  Button,
-  RowWrapper,
-  Separator,
-  TitleRowWrapper,
-  Wrapper,
-} from './styled';
+import { ButtonWrapper, LinkCell, TableRow, Wrapper } from './styled';
 
 const propTypes = {
   history: PropTypes.shape({
@@ -20,38 +23,74 @@ const propTypes = {
   }).isRequired,
 };
 
-const renderRows = (arr, onClick) =>
-  arr.map(d => (
-    <RowWrapper
-      href={`articles/${d._id}`}
-      key={d._id}
-      onClick={e => onClick(e, d._id)}
-    >
-      <Row
-        topic={d.topic}
-        subTopic={d.sub_topic}
-        title={d.title}
-        creator={d.creator.username}
-      />
-    </RowWrapper>
-  ));
+const CREATOR = 'creator';
+
+const columnData = [
+  { id: 'topic', numeric: false, label: 'Topic' },
+  { id: 'sub_topic', numeric: false, label: 'Sub Topic' },
+  { id: 'title', numeric: false, label: 'Title' },
+  { id: CREATOR, numeric: false, label: 'Owner' },
+];
 
 class Articles extends React.Component {
   state = {
     data: [],
+    order: 'asc',
   };
+
   componentDidMount = () => {
-    actions.getAll().then(data => this.setState({ data }));
+    this.fetchData().then(data => this.setState({ data }));
   };
-  handleClick = (e, id) => {
-    e.preventDefault();
-    const { name } = e.target.dataset;
-    if (name === dataNames.creator) {
-      const articles = this.state.data.filter(row => row._id === id);
-      const uid = articles[0].creator._id;
-      this.props.history.push(`/users/${uid}`);
+
+  // eslint-disable-next-line
+  fetchData() {
+    return actions.getAll();
+  }
+
+  handleSort = id => {
+    const { data, sortedBy } = this.state;
+    let { order } = this.state;
+
+    if (id !== sortedBy) {
+      order = 'desc';
+    }
+
+    if (id === CREATOR) {
+      data.sort((a, b) => {
+        if (a[id].username.toLowerCase() < b[id].username.toLowerCase())
+          return order === 'asc' ? 1 : -1;
+        if (a[id].username.toLowerCase() > b[id].username.toLowerCase())
+          return order === 'asc' ? -1 : 1;
+        return 0;
+      });
     } else {
-      this.props.history.push(`${this.props.location.pathname}/${id}`);
+      data.sort((a, b) => {
+        if (a[id].toLowerCase() < b[id].toLowerCase())
+          return order === 'asc' ? 1 : -1;
+        if (a[id].toLowerCase() > b[id].toLowerCase())
+          return order === 'asc' ? -1 : 1;
+        return 0;
+      });
+    }
+
+    const newOrder = order === 'asc' && sortedBy === id ? 'desc' : 'asc';
+
+    this.setState(() => ({
+      data,
+      order: newOrder,
+      sortedBy: id,
+    }));
+  };
+
+  handleClick = (e, articleId, creatorId) => {
+    e.preventDefault();
+
+    const { name } = e.target.dataset;
+
+    if (name === CREATOR) {
+      this.props.history.push(`/users/${creatorId}`);
+    } else {
+      this.props.history.push(`${this.props.location.pathname}/${articleId}`);
     }
   };
 
@@ -60,19 +99,51 @@ class Articles extends React.Component {
   };
 
   render() {
+    const { data, order, sortedBy } = this.state;
+
     return (
       <Wrapper>
-        <Button onClick={this.handleNewArticle}>New Article</Button>
-        <TitleRowWrapper>
-          <Row
-            topic="Topic"
-            subTopic="Sub Topic"
-            title="Title"
-            creator="Owner"
-          />
-        </TitleRowWrapper>
-        <Separator />
-        {renderRows(this.state.data, this.handleClick)}
+        <ButtonWrapper>
+          <Button
+            variant="raised"
+            color="primary"
+            onClick={this.handleNewArticle}
+          >
+            New Article
+          </Button>
+        </ButtonWrapper>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {columnData.map(column => (
+                  <TableCell key={column.id} numeric={column.numeric}>
+                    <TableSortLabel
+                      active={column.id === sortedBy}
+                      direction={order}
+                      onClick={() => this.handleSort(column.id)}
+                    >
+                      {column.label}
+                    </TableSortLabel>
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map(d => (
+                <TableRow
+                  key={d._id}
+                  onClick={e => this.handleClick(e, d._id, d.creator._id)}
+                >
+                  <TableCell>{d.topic}</TableCell>
+                  <TableCell>{d.sub_topic}</TableCell>
+                  <TableCell>{d.title}</TableCell>
+                  <LinkCell data-name={CREATOR}>{d.creator.username}</LinkCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
       </Wrapper>
     );
   }
@@ -81,5 +152,3 @@ class Articles extends React.Component {
 Articles.propTypes = propTypes;
 
 export default Articles;
-
-export { renderRows };
