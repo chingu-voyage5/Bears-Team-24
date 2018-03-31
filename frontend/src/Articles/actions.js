37f7e8bdcc4357ebf7ca8b51373c54acd9936751
Viktors Jenovs <jviktors@gmail.com>
2018-03-31 14:54:29 +0300

    Update actions to work in standalone mode

--- frontend/src/Articles/actions.js
+++ frontend/src/Articles/actions.js
@@ -1,8 +1,21 @@
-const getAll = () =>
-  fetch('/api/v0/articles', {
-    method: 'get',
-    headers: { accept: 'application/json', 'content-type': 'application/json' },
-    credentials: 'same-origin',
-  }).then(res => res.json());
+let getAll;
+
+if (process.env.REACT_APP_ALONE) {
+  /* eslint-disable */
+  const articles = require('../_mockData/articles.json');
+  /* eslint-enable */
+
+  getAll = () => new Promise(resolve => resolve(articles));
+} else {
+  getAll = () =>
+    fetch('/api/v0/articles', {
+      method: 'get',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      credentials: 'same-origin',
+    }).then(res => res.json());
+}
 
 export default { getAll };
