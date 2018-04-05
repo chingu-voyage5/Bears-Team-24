22ee8433851d1b0d6cae69eb76257e72eafb0c0e
Viktors Jenovs <jviktors@gmail.com>
2018-04-05 15:52:03 +0300

    Make actions work as standalone

--- frontend/src/Assets/actions.js
+++ frontend/src/Assets/actions.js
@@ -1,8 +1,21 @@
-const getAll = () =>
-  fetch('/api/v1/assets', {
-    method: 'get',
-    headers: { accept: 'application/json', 'content-type': 'application/json' },
-    credentials: 'same-origin',
-  }).then(res => res.json());
+let getAll;
+
+if (process.env.REACT_APP_ALONE) {
+  /* eslint-disable */
+  const assets = require('../_mockData/assets.json');
+  /* eslint-enable */
+
+  getAll = () => new Promise(resolve => resolve(assets));
+} else {
+  getAll = () =>
+    fetch('/api/v1/assets', {
+      method: 'get',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      credentials: 'same-origin',
+    }).then(res => res.json());
+}
 
 export default { getAll };
