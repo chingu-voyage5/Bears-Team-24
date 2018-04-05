d43eb36668a9f1b7a66a49dab1fa1bb7dde695ca
Viktors Jenovs <jviktors@gmail.com>
2018-04-05 15:52:54 +0300

    Refactor Assets to use Material-UI

--- frontend/src/Assets/Assets.js
+++ frontend/src/Assets/Assets.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactTable from 'react-table';
+
+// Material UI components
+import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableSortLabel,
+} from 'material-ui/Table';
+
 import actions from './actions';
+import { ButtonWrapper, LinkCell, TableRow, Wrapper } from './styled';
 
-import { Button, Wrapper } from './styled';
+const CREATOR = 'creator';
+const CREATED = 'created';
 
-import '../react-table.css';
-
-import columns from './columns_config';
+const columnData = [
+  { id: 'content_type', numeric: false, label: 'Type' },
+  { id: CREATOR, numeric: false, label: 'Owner' },
+  { id: 'title', numeric: false, label: 'Title' },
+  { id: 'created', numeric: false, label: 'Date created' },
+];
 
 const propTypes = {
   history: PropTypes.shape({
@@ -21,38 +36,122 @@ const propTypes = {
 class Assets extends React.Component {
   state = {
     data: [],
+    order: 'asc',
   };
+
   componentDidMount = () => {
-    actions.getAll().then(data => {
-      this.setState({ data });
-    });
+    actions
+      .getAll()
+      .then(data => {
+        this.setState({ data });
+      })
+      // eslint-disable-next-line
+      .catch(err => console.log(err));
   };
-  handleClick = () => {
+
+  handleClick = (e, assetId, creatorId) => {
+    e.preventDefault();
+
+    const { name } = e.target.dataset;
+
+    if (name === CREATOR) {
+      this.props.history.push(`/users/${creatorId}`);
+    } else {
+      this.props.history.push(`${this.props.location.pathname}/${assetId}`);
+    }
+  };
+
+  handleSort = id => {
+    const { data, sortedBy } = this.state;
+    let { order } = this.state;
+
+    if (id !== sortedBy) {
+      order = 'desc';
+    }
+
+    if (id === CREATOR) {
+      data.sort((a, b) => {
+        if (a[id].username.toLowerCase() < b[id].username.toLowerCase())
+          return order === 'asc' ? 1 : -1;
+        if (a[id].username.toLowerCase() > b[id].username.toLowerCase())
+          return order === 'asc' ? -1 : 1;
+        return 0;
+      });
+    } else if (id === CREATED) {
+      data.sort((a, b) => {
+        if (a[id] < b[id]) return order === 'asc' ? 1 : -1;
+        if (a[id] > b[id]) return order === 'asc' ? -1 : 1;
+        return 0;
+      });
+    } else {
+      data.sort((a, b) => {
+        if (a[id].toLowerCase() < b[id].toLowerCase())
+          return order === 'asc' ? 1 : -1;
+        if (a[id].toLowerCase() > b[id].toLowerCase())
+          return order === 'asc' ? -1 : 1;
+        return 0;
+      });
+    }
+
+    const newOrder = order === 'asc' && sortedBy === id ? 'desc' : 'asc';
+
+    this.setState(() => ({
+      data,
+      order: newOrder,
+      sortedBy: id,
+    }));
+  };
+
+  handleNewAsset = () => {
     this.props.history.push(`${this.props.location.pathname}/new`);
   };
 
-  handleNavigation = id => {
-    this.props.history.push(`${this.props.location.pathname}/${id}`);
-  };
-
   render() {
-    const { data } = this.state;
+    const { data, order, sortedBy } = this.state;
+
     return (
       <Wrapper>
-        <Button onClick={this.handleClick}>New Asset</Button>
-        <ReactTable
-          data={data}
-          columns={columns}
-          noDataText="No data found."
-          className={data.length ? '-striped -highlight' : ''}
-          getTdProps={(_, rowInfo) => ({
-            onClick: () => {
-              if (rowInfo) {
-                this.handleNavigation(rowInfo.row._id);
-              }
-            },
-          })}
-        />
+        <ButtonWrapper>
+          <Button
+            variant="raised"
+            color="primary"
+            onClick={this.handleNewAsset}
+          >
+            New Asset
+          </Button>
+        </ButtonWrapper>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {columnData.map(column => (
+                  <TableCell key={column.id} numeric={column.numeric}>
+                    <TableSortLabel
+                      active={column.id === sortedBy}
+                      direction={order}
+                      onClick={() => this.handleSort(column.id)}
+                    >
+                      {column.label}
+                    </TableSortLabel>
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map(d => (
+                <TableRow
+                  key={d._id}
+                  onClick={e => this.handleClick(e, d._id, d.creator._id)}
+                >
+                  <TableCell>{d.content_type}</TableCell>
+                  <LinkCell data-name={CREATOR}>{d.creator.username}</LinkCell>
+                  <TableCell>{d.title}</TableCell>
+                  <TableCell>{new Date(d.created).toUTCString()}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
       </Wrapper>
     );
   }
