e2593127060138e45f44328903a02fdc24f0220d
Viktors Jenovs <jviktors@gmail.com>
2018-04-10 17:08:34 +0300

    Update tests

--- frontend/src/ArticleEdit/ArticleEdit.test.js
+++ frontend/src/ArticleEdit/ArticleEdit.test.js
@@ -1,44 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import 'jest-styled-components';
 
 import ArticleEdit from '.';
 
-describe('ArticleEdit component', () => {
-  it('renders edit article page', () => {
-    const wrapper = shallow(<ArticleEdit />);
+const article = {
+  _id: '43039ac8d0a244719d9d31e0731bcbe8',
+  active: true,
+  created: '1498771528',
+  creator: '85e026bbd79b46ce990dbb956490e8d0',
+  contributor_list: [
+    '7326fe8cff7242d094815c184aa725dd',
+    'd47d214bf9f64df786b295efa9b1b45e',
+    'ee2686b1d7e84c3dae4db8bd00ea8d2a',
+  ],
+  title: 'Bradypus variegatus',
+  topic: 'Pilosa',
+  sub_topic: 'Folivora',
+  content:
+    '## Lorem\n**ipsum** dolor _sit amet_, ~~consectetuer~~ adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+};
 
-    expect(
-      wrapper
-        .find('Heading1')
-        .children()
-        .text()
-    ).toBe('Edit article');
+describe('ArticleEdit', () => {
+  it('should match snapshot', () => {
+    jest
+      .spyOn(ArticleEdit.prototype, 'fetchData')
+      .mockImplementation(() => new Promise(resolve => resolve(article)));
 
-    expect(wrapper.find('Input').length).toBe(3);
-    expect(wrapper.find('Editor').length).toBe(1);
-    expect(wrapper.find('Tab').length).toBe(2);
-    expect(wrapper.find('Textarea').length).toBe(1);
-    expect(wrapper.find('Preview').length).toBe(0);
+    const wrapper = shallow(<ArticleEdit id="123abc" />);
 
-    wrapper.setState({ article: { title: 'some title' } });
-
-    expect(
-      wrapper
-        .find('Input')
-        .at(0)
-        .props().value.length
-    ).not.toBe(0);
+    expect(wrapper).toMatchSnapshot();
   });
 
-  it('renders content Preview', () => {
-    const wrapper = shallow(<ArticleEdit />);
+  it('empty component should match snapshot', () => {
+    const wrapper = shallow(<ArticleEdit empty />);
 
-    expect(wrapper.find('Textarea').length).toBe(1);
-    expect(wrapper.find('Preview').length).toBe(0);
-
-    wrapper.setState({ edit: false, article: { content: 'some content' } });
-
-    expect(wrapper.find('Textarea').length).toBe(0);
-    expect(wrapper.find('Preview').length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
   });
 });
