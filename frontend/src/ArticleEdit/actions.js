20165a1d52515090a44289f5514dd10d15f7345d
Viktors Jenovs <jviktors@gmail.com>
2018-04-10 16:54:47 +0300

    Refactor actions to work in standalone mode

--- frontend/src/ArticleEdit/actions.js
+++ frontend/src/ArticleEdit/actions.js
@@ -1,16 +1,35 @@
-const get = id =>
-  fetch(`/api/v0/articles/${id}`, {
-    method: 'get',
-    headers: { accept: 'application/json', 'content-type': 'application/json' },
-    credentials: 'same-origin',
-  }).then(res => res.json());
+let get;
+let save;
 
-const save = payload =>
-  fetch('/api/v0/articles', {
-    method: 'post',
-    headers: { accept: 'application/json', 'content-type': 'application/json' },
-    credentials: 'same-origin',
-    body: JSON.stringify(payload),
-  }).then(res => res.json());
+if (process.env.REACT_APP_ALONE) {
+  /* eslint-disable */
+  const article = require('../_mockData/article.json');
+  /* eslint-enable */
+
+  get = () => new Promise(resolve => resolve(article));
+
+  save = payload => new Promise(resolve => resolve(payload));
+} else {
+  get = id =>
+    fetch(`/api/v0/articles/${id}`, {
+      method: 'get',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      credentials: 'same-origin',
+    }).then(res => res.json());
+
+  save = payload =>
+    fetch('/api/v0/articles', {
+      method: 'post',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify(payload),
+    }).then(res => res.json());
+}
 
 export default { get, save };
