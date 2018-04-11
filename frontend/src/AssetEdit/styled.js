7f538c6e9af183d49eee724c06c71d287ad41256
Viktors Jenovs <jviktors@gmail.com>
2018-04-11 19:54:55 +0300

    Code cleanup and housekeeping

--- frontend/src/AssetEdit/styled.js
+++ frontend/src/AssetEdit/styled.js
@@ -1,34 +1,6 @@
 import styled from 'styled-components';
 import Typography from 'material-ui/Typography';
 
-export const Button = styled.button`
-  background-color: #15df89;
-  border: none;
-  border-radius: 12px;
-  box-shadow: 0 1px 2px #aaa;
-  color: #fff;
-  cursor: pointer;
-  display: block;
-  font-size: 1.2rem;
-  font-weight: bold;
-  height: auto;
-  margin: 1rem;
-  max-width: 10rem;
-  padding: 0.5rem 1.2rem;
-  position: relative;
-
-  &:hover {
-    background-color: #08d27c;
-  }
-
-  &:active {
-    box-shadow: none;
-    top: 1px;
-  }
-`;
-
-Button.displayName = 'Button';
-
 export const DropArea = styled.button`
   background-color: #e2e2e2;
   border: none;
@@ -41,7 +13,6 @@ export const DropArea = styled.button`
   position: relative;
   width: 100%;
 `;
-
 DropArea.displayName = 'DropArea';
 
 export const Hint = styled.p`
@@ -63,7 +34,6 @@ export const ImgPreview = styled.img`
   transform: ${p => (p.scaled ? 'scale(2) translateY(25%)' : 'scale(1)')};
   transition: transform 0.15s ease;
 `;
-
 ImgPreview.displayName = 'ImgPreview';
 
 export const InvisibleInput = styled.input.attrs({
@@ -71,26 +41,7 @@ export const InvisibleInput = styled.input.attrs({
 })`
   display: none;
 `;
-
 InvisibleInput.displayName = 'InvisibleInput';
-
-export const InputField = styled.p`
-  align-items: center;
-  background-color: #e2e2e2;
-  border: 1px solid gray;
-  display: flex;
-  flex: 1;
-  font-size: 1.1rem;
-  font-weight: normal;
-  height: 100%;
-  line-height: 1.1rem;
-  margin-left: 2rem;
-  margin-right: 30%;
-  max-width: 50%;
-  padding-left: 0.5rem;
-`;
-
-InputField.displayName = 'InputField';
 
 export const Label = styled.div`
   font-weight: bold;
@@ -100,7 +51,6 @@ export const Label = styled.div`
     min-width: 25%;
   }
 `;
-
 Label.displayName = 'Label';
 
 export const Wrapper = styled.div`
