01f0d7c0f5a5650a73cd24876740298cbac60ac0
nikrb <nik_rb@yahoo.com>
2018-04-27 11:43:26 +0100

    fix: only build sidebar tree one on startup

--- frontend/src/Sidebar/Sidebar.js
+++ frontend/src/Sidebar/Sidebar.js
@@ -21,23 +21,27 @@ export default class Sidebar extends React.Component {
   componentWillReceiveProps(nextProps) {
     const { articles } = nextProps;
     let selectedArticlePath = [];
-    if (nextProps.match.params.id) {
-      const { id } = nextProps.match.params;
-      const selectedArticles = articles.filter(article => article._id === id);
-      if (selectedArticles.length) {
-        const { topic, sub_topic, title } = selectedArticles[0];
-        selectedArticlePath = [topic.name, title];
-        // eslint-disable-next-line camelcase
-        if (sub_topic) {
-          selectedArticlePath = selectedArticlePath.concat(
-            sub_topic.name.split('>')
-          );
+    // only build the sidebar tree once on startup, otherwise the tree is
+    // collapsed everytime an article is selected/viewed
+    if (this.state.articlesHtml.length === 0) {
+      if (nextProps.match.params.id) {
+        const { id } = nextProps.match.params;
+        const selectedArticles = articles.filter(article => article._id === id);
+        if (selectedArticles.length) {
+          const { topic, sub_topic, title } = selectedArticles[0];
+          selectedArticlePath = [topic.name, title];
+          // eslint-disable-next-line camelcase
+          if (sub_topic) {
+            selectedArticlePath = selectedArticlePath.concat(
+              sub_topic.name.split('>')
+            );
+          }
         }
       }
+      const tree = getTree(articles);
+      const articlesHtml = getChildren(tree, selectedArticlePath);
+      this.setState({ articlesHtml });
     }
-    const tree = getTree(articles);
-    const articlesHtml = getChildren(tree, selectedArticlePath);
-    this.setState({ articlesHtml });
   }
 
   render() {
