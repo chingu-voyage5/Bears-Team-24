602b18e1fe1de2bb4e612d7b151c878845f35fff
nikrb <nik_rb@yahoo.com>
2018-05-05 00:13:50 +0100

    test: ArticleEdit ContentEdit

---
+++ frontend/src/ArticleEdit/ContentEdit.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
+
+import ContentEdit from './ContentEdit';
+
+it('renders ContentEdit Form', () => {
+  const handleFieldChange = jest.fn();
+  const comp = renderer.create(
+    <ContentEdit content="test content" handleFieldChange={handleFieldChange} />
+  );
+  const tree = comp.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('changes tabs', () => {
+  const handleFieldChange = jest.fn();
+  const comp = mount(
+    <ContentEdit content="test content" handleFieldChange={handleFieldChange} />
+  );
+  const inst = comp.instance();
+  inst.handleTabSwitch(null, 1);
+  expect(inst.state.edit).toBe(1);
+  return Promise.resolve().then(() => {
+    comp.update();
+    expect(
+      comp
+        .find('Preview')
+        .at(0)
+        .childAt(0)
+        .text()
+    ).toBe(`test content
+`);
+  });
+});
