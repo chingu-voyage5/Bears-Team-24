4933e0f8b504cb5b34666fd20ff8e9d20f093e3c
nikrb <nik_rb@yahoo.com>
2018-06-17 22:25:10 +0100

    test: fix BE isLoggedInAdmin

--- backend/routes/auth/isLoggedInAdmin.js
+++ backend/routes/auth/isLoggedInAdmin.js
@@ -1,7 +1,6 @@
 function isLoggedInAdmin(req, res, next) {
   const isTest = process.env.NODE_ENV === 'test';
-  const hasAdminRole = req.user.role === 'admin';
-  if (isTest || (req.isAuthenticated() && hasAdminRole)) {
+  if (isTest || (req.isAuthenticated() && req.user.role === 'admin')) {
     return next();
   }
   return res.sendStatus(401);
