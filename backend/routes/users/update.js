8d3eebfc07ab4cf730d7959093bb6a5699fd10a5
nikrb <nik_rb@yahoo.com>
2018-06-23 22:10:41 +0100

    add admin user detail save detail endpoint and missing fields to user update route

--- backend/routes/users/update.js
+++ backend/routes/users/update.js
@@ -2,18 +2,19 @@
 const User = require('../../models/user');
 
 async function update(req, res) {
-  const user = await User.findById(req.user._id);
-  const { name, email } = req.body;
+  const user = await User.findById(req.body._id);
+  const { name, email, role, bio } = req.body;
   user.name = name;
   user.email = email;
+  user.role = role;
+  user.bio = bio;
 
   try {
     await user.save();
   } catch (e) {
-    res.json({ success: false, message: e });
-    return;
+    return res.json({ success: false, message: e });
   }
-  res.json({ success: true, user });
+  return res.json({ success: true, user });
 }
 
 module.exports = update;
