4a71cd9feb715eb0c0b19929c4c7aa58718a618a
Viktors Jenovs <jviktors@gmail.com>
2018-07-23 09:38:51 +0300

    Add SearchResults component to CMSContainer

--- frontend/src/CMSContainer/CMSContainer.js
+++ frontend/src/CMSContainer/CMSContainer.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import { getArticleList, searchArticles } from './actions';
 import ContentArea from '../ContentArea';
+import { Highlight } from '../SearchResults/styled';
 import Search from '../Search';
+import SearchResults from '../SearchResults';
 import Sidebar from '../Sidebar';
-import { ContentWrapper, Wrapper } from './styled';
+import { ContentWrapper, SearchStatus, Wrapper } from './styled';
 
 export default class CMSContainer extends React.Component {
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+  };
+
   state = {
     articleList: [],
     searchResults: [],
+    showSearchResults: false,
   };
 
   componentDidMount = () => {
@@ -23,37 +33,79 @@ export default class CMSContainer extends React.Component {
     return getArticleList();
   }
 
+  handleSearchClick = id => {
+    this.props.history.push(`/cms/${id}`);
+    this.setState(() => ({
+      showSearchResults: false,
+    }));
+  };
+
   handleSearch = query => {
     searchArticles(query).then(searchResults => {
-      this.setState(() => ({ searchResults, searchQuery: query }));
+      this.setState(() => ({
+        searchResults,
+        searchQuery: query,
+        showSearchResults: true,
+      }));
     });
   };
 
-  render() {
-    const { articleList, searchResults } = this.state;
+  hideSearchResults = () => {
+    this.setState(() => ({
+      showSearchResults: false,
+    }));
+  };
 
-    // TEMP log search results to the console.
-    // eslint-disable-next-line
-    console.log(searchResults);
+  render() {
+    const {
+      articleList,
+      searchQuery,
+      searchResults,
+      showSearchResults,
+    } = this.state;
 
     return (
       <Wrapper>
         <Route
           path="/cms/:id?"
-          render={p => <Sidebar {...p} articles={articleList} />}
+          render={p => (
+            <Sidebar
+              {...p}
+              articles={articleList}
+              onClick={this.hideSearchResults}
+            />
+          )}
         />
         <ContentWrapper>
           <Search onSubmit={this.handleSearch} />
-          <Route
-            exact
-            path="/cms"
-            render={() => (
-              <div>
-                <h1>Welcome</h1>
-              </div>
-            )}
-          />
-          <Route path="/cms/:id" component={ContentArea} />
+          {showSearchResults ? (
+            <Fragment>
+              <SearchStatus>
+                {searchResults.length
+                  ? `Found ${searchResults.length} topics containing `
+                  : `Nothing found for: `}
+                <Highlight bold>{searchQuery}</Highlight>
+              </SearchStatus>
+              <SearchResults
+                results={searchResults}
+                query={searchQuery}
+                onClick={this.handleSearchClick}
+              />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Route
+                exact
+                path="/cms"
+                render={() => (
+                  <div>
+                    <h1>Welcome</h1>
+                  </div>
+                )}
+              />
+              <Route path="/cms/:id" component={ContentArea} />
+            </Fragment>
+          )}
         </ContentWrapper>
       </Wrapper>
     );
--- frontend/src/CMSContainer/styled.js
+++ frontend/src/CMSContainer/styled.js
@@ -6,6 +6,12 @@ export const ContentWrapper = styled.div`
   width: 100%;
 `;
 
+export const SearchStatus = styled.div`
+  margin-top: 1rem;
+  text-align: center;
+  width: 100%;
+`;
+
 export const Wrapper = styled.div`
   display: flex;
   padding-top: 10px;
